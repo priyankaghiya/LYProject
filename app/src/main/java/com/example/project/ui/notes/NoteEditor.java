78a8586d950a884a28980a584d37876da2fac4b8
Priyanka <piyughiya12@gmail.com>
2020-10-14 15:38:56 +0530

    Working on Profile and Notes

---
+++ app/src/main/java/com/example/project/ui/notes/NoteEditor.java
@@ -0,0 +1,40 @@
+package com.example.project.ui.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.project.MainActivity;
+import com.example.project.R;
+
+public class NoteEditor extends AppCompatActivity
+{
+
+    //initialize component
+    EditText note_et;
+    int noteId;
+    //complete
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+        //code
+        note_et=(EditText)findViewById(R.id.note_et);
+
+        Intent intent=getIntent();
+        noteId=intent.getIntExtra("noteId",-1);
+
+        if(noteId!=-1)
+        {
+            note_et.setText(NotesFragment.notes.get(noteId));
+        }
+
+
+        //complete
+
+    }
+}
---
+++ app/src/main/java/com/example/project/ui/notes/NotesViewModel.java
@@ -0,0 +1,13 @@
+package com.example.project.ui.notes;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotesViewModel extends ViewModel
+{
+
+    private MutableLiveData<String> mText;
+
+
+}
